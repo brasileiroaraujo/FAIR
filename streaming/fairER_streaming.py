fa969d802f0f52b044649585497320ad49540602
Tiago Brasileiro <brasileiroaraujo@gmail.com>
2023-08-16 10:26:59 -0300

    Add conditions to datasets

--- streaming/fairER_streaming.py
+++ streaming/fairER_streaming.py
@@ -50,6 +50,29 @@ def extractLeftColumns(df, data):
         df_splited['left_authors'] = df_splited['left_authors'].str.split("VAL", expand=True)[1]
         df_splited['left_venue'] = df_splited['left_venue'].str.split("VAL", expand=True)[1]
         df_splited['left_year'] = df_splited['left_year'].str.split("VAL", expand=True)[1]
+    elif data == 'DBLP-GoogleScholar':
+        df_splited.rename(columns={1: 'left_title', 2: 'left_authors', 3: 'left_venue', 4: 'left_year'}, inplace=True)
+        df_splited['left_title'] = df_splited['left_title'].str.split("VAL", expand=True)[1]
+        df_splited['left_authors'] = df_splited['left_authors'].str.split("VAL", expand=True)[1]
+        df_splited['left_venue'] = df_splited['left_venue'].str.split("VAL", expand=True)[1]
+        df_splited['left_year'] = df_splited['left_year'].str.split("VAL", expand=True)[1]
+    elif data == 'iTunes-Amazon':
+        df_splited.rename(columns={1: 'left_Song_Name', 2: 'left_Artist_Name', 3: 'left_Album_Name', 4: 'left_Genre', 5: 'left_Price', 6:'left_Copyleft', 7:'left_Time', 8:'left_Released'}, inplace=True)
+        df_splited['left_Song_Name'] = df_splited['left_Song_Name'].str.split("VAL", expand=True)[1]
+        df_splited['left_Artist_Name'] = df_splited['left_Artist_Name'].str.split("VAL", expand=True)[1]
+        df_splited['left_Album_Name'] = df_splited['left_Album_Name'].str.split("VAL", expand=True)[1]
+        df_splited['left_Genre'] = df_splited['left_Genre'].str.split("VAL", expand=True)[1]
+        df_splited['left_Price'] = df_splited['left_Price'].str.split("VAL", expand=True)[1]
+        df_splited['left_Copyleft'] = df_splited['left_Copyleft'].str.split("VAL", expand=True)[1]
+        df_splited['left_Time'] = df_splited['left_Time'].str.split("VAL", expand=True)[1]
+        df_splited['left_Released'] = df_splited['left_Released'].str.split("VAL", expand=True)[1]
+    elif data == 'Walmart-Amazon':
+        df_splited.rename(columns={1: 'left_title', 2: 'left_category', 3: 'left_brand', 4: 'left_modelno', 5: 'left_price'}, inplace=True)
+        df_splited['left_title'] = df_splited['left_title'].str.split("VAL", expand=True)[1]
+        df_splited['left_category'] = df_splited['left_category'].str.split("VAL", expand=True)[1]
+        df_splited['left_brand'] = df_splited['left_brand'].str.split("VAL", expand=True)[1]
+        df_splited['left_modelno'] = df_splited['left_modelno'].str.split("VAL", expand=True)[1]
+        df_splited['left_price'] = df_splited['left_price'].str.split("VAL", expand=True)[1]
     else:
         print("NEED TO IMPLEMENT CONDITION")
 
@@ -81,6 +104,29 @@ def extractRightColumns(df, data):
         df_splited['right_authors'] = df_splited['right_authors'].str.split("VAL", expand=True)[1]
         df_splited['right_venue'] = df_splited['right_venue'].str.split("VAL", expand=True)[1]
         df_splited['right_year'] = df_splited['right_year'].str.split("VAL", expand=True)[1]
+    elif data == 'DBLP-GoogleScholar':
+        df_splited.rename(columns={1: 'right_title', 2: 'right_authors', 3: 'right_venue', 4: 'right_year'}, inplace=True)
+        df_splited['right_title'] = df_splited['right_title'].str.split("VAL", expand=True)[1]
+        df_splited['right_authors'] = df_splited['right_authors'].str.split("VAL", expand=True)[1]
+        df_splited['right_venue'] = df_splited['right_venue'].str.split("VAL", expand=True)[1]
+        df_splited['right_year'] = df_splited['right_year'].str.split("VAL", expand=True)[1]
+    elif data == 'iTunes-Amazon':
+        df_splited.rename(columns={1: 'right_Song_Name', 2: 'right_Artist_Name', 3: 'right_Album_Name', 4: 'right_Genre', 5: 'right_Price', 6:'right_CopyRight', 7:'right_Time', 8:'right_Released'}, inplace=True)
+        df_splited['right_Song_Name'] = df_splited['right_Song_Name'].str.split("VAL", expand=True)[1]
+        df_splited['right_Artist_Name'] = df_splited['right_Artist_Name'].str.split("VAL", expand=True)[1]
+        df_splited['right_Album_Name'] = df_splited['right_Album_Name'].str.split("VAL", expand=True)[1]
+        df_splited['right_Genre'] = df_splited['right_Genre'].str.split("VAL", expand=True)[1]
+        df_splited['right_Price'] = df_splited['right_Price'].str.split("VAL", expand=True)[1]
+        df_splited['right_CopyRight'] = df_splited['right_CopyRight'].str.split("VAL", expand=True)[1]
+        df_splited['right_Time'] = df_splited['right_Time'].str.split("VAL", expand=True)[1]
+        df_splited['right_Released'] = df_splited['right_Released'].str.split("VAL", expand=True)[1]
+    elif data == 'Walmart-Amazon':
+        df_splited.rename(columns={1: 'right_title', 2: 'right_category', 3: 'right_brand', 4: 'right_modelno', 5: 'right_price'}, inplace=True)
+        df_splited['right_title'] = df_splited['right_title'].str.split("VAL", expand=True)[1]
+        df_splited['right_category'] = df_splited['right_category'].str.split("VAL", expand=True)[1]
+        df_splited['right_brand'] = df_splited['right_brand'].str.split("VAL", expand=True)[1]
+        df_splited['right_modelno'] = df_splited['right_modelno'].str.split("VAL", expand=True)[1]
+        df_splited['right_price'] = df_splited['right_price'].str.split("VAL", expand=True)[1]
     else:
         print("NEED TO IMPLEMENT CONDITION")
 
