b121607233df9bf8dcf15db453933942f58c5d84
Tiago Brasileiro <brasileiroaraujo@gmail.com>
2023-10-03 17:11:17 -0300

    Testing a multiple group [Issue#4]

--- clustering/fair_unique_mapping_clustering.py
+++ clustering/fair_unique_mapping_clustering.py
@@ -95,9 +95,10 @@ def run_steraming_ranking_by_groups(candidates, nextGroup, results_limit):
     #print(nonprotected_candidates)
 
     while (groups_indexes) and (len(matches) < results_limit): #if groups_indexes is empty, means all groups are completely used
-        if grouped_candidates.get(groups_indexes[nextGroup]) == None:
+        if grouped_candidates.get(groups_indexes[nextGroup]) == None or len(grouped_candidates.get(groups_indexes[nextGroup])) == 0:
             groups_indexes.pop(nextGroup)
             nextGroup = 0 if nextGroup == (len(groups_indexes)-1) else nextGroup+1
+            continue
 
         cand = grouped_candidates.get(groups_indexes[nextGroup]).pop(0)
 
