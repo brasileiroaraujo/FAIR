3fac016f01a5ab0120188a1e93d7db4889726492
Tiago Brasileiro <brasileiroaraujo@gmail.com>
2023-10-30 21:49:47 -0300

    Metrics: correction #4

--- util.py
+++ util.py
@@ -28,6 +28,8 @@ def pair_is_protected(tuple=None, dataset=None, return_condition=False, explanat
                 return eval(default_conditions_w_exp[dataset]) if methods.protectedCond(dataset,1) is None else eval(methods.protectedCond(dataset,1))
         except AttributeError:
             if dataset == 'DBLP-ACM':
+                print("2 a_l: ", str(tuple.left_authors))
+                print("2 a_r: ", str(tuple.right_authors))
                 last_author_fname_l = str(tuple.left_authors).split(
                     ",")[-1].strip().split(" ")[0].replace('.', '')
                 last_author_fname_r = str(tuple.right_authors).split(
@@ -37,6 +39,7 @@ def pair_is_protected(tuple=None, dataset=None, return_condition=False, explanat
 
                 print("2 name: ", last_author_fname_l)
                 print("2 name: ", last_author_fname_r)
+                print("2 gender: ", last_author_is_female)
 
                 return last_author_is_female
             else:
