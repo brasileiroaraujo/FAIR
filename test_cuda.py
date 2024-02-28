419ca77a5af643565bc3cb48e68ed84190a3f789
Tiago Brasileiro <brasileiroaraujo@gmail.com>
2024-02-28 18:55:17 -0300

    Including GNEM matcher [correction #14]

--- test_cuda.py
+++ test_cuda.py
@@ -1,4 +1,17 @@
-import torch
+# import torch
+#
+# print(torch.__version__)
+# print(torch.cuda.is_available())
+import pandas as pd
 
-print(torch.__version__)
-print(torch.cuda.is_available())
+pairs_to_compare = pd.read_csv("data/er_magellan/Structured/Amazon-Google/test.txt", header=None, sep='\n')
+lines = pairs_to_compare.loc[0:100]
+
+for i in lines.to_numpy():
+    i = i[0]
+    print("line ", i)
+
+# row1 = str(pairs_to_compare[0].iloc[0])
+# print(row1)
+# useful_field_num = int(str(pairs_to_compare[0].iloc[0]).count('COL')/2) #TODO: VALIDATE THIS COMPUTATION
+# print("useful_field_num ", useful_field_num)
