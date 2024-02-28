47c7fff208508031ebce612ff9bf45ca7791c641
Tiago Brasileiro <brasileiroaraujo@gmail.com>
2024-02-28 00:07:23 -0300

    Including GNEM matcher [correction #12]

--- SAFER_run_gpu.py
+++ SAFER_run_gpu.py
@@ -201,7 +201,7 @@ def main(args):
         print('DITTO SELECTED')
         config, model, threshold, summarizer, dk_injector = setUpDitto(task="Structured/" + task, lm=lm, checkpoint_path="checkpoints/", threshold = threshold)
     elif (matching_algorithm == 'gnem'):
-        useful_field_num = pairs_to_compare.iloc[0].count('COL')/2 #TODO: VALIDATE THIS COMPUTATION
+        useful_field_num = int(str(pairs_to_compare[0].iloc[0]).count('COL')/2)
         print("useful_field_num ", useful_field_num)
         model, embed_model, criterion = setUpGNEM(data=task, useful_field_num=useful_field_num)
         print('GNEM SELECTED')
